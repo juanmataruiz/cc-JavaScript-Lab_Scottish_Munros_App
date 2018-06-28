19eb9ef535481880eba3c14050278e9215499b61
Juan <juanmataruiz84@gmail.com>
2018-06-28 15:47:38 +0100

    add promises

--- src/helpers/request_helper.js
+++ src/helpers/request_helper.js
@@ -3,17 +3,10 @@ const RequestHelper = function (url) {
 }
 
 RequestHelper.prototype.get = function (onComplete) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', this.url);
-  xhr.addEventListener('load', function() {
-    if(this.status !== 200){
-      console.error(this)
-      return;
-    }
-    const data = JSON.parse(this.responseText);
-    onComplete(data);
-  });
-  xhr.send();
+  fetch(this.url)
+    .then(result => result.json())
+    .then(json => onComplete(json))
+    .catch((error) => console.error(error))
 };
 
 module.exports = RequestHelper;
