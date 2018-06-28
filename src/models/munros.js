c2e57fce97d4ce449a865661ec1c030b62767343
Juan <juanmataruiz84@gmail.com>
2018-06-28 14:45:20 +0100

    dropdown populated

--- src/models/munros.js
+++ src/models/munros.js
@@ -1,17 +1,28 @@
 const PubSub = require('../helpers/pub_sub.js');
+const RequestHelper = require('../helpers/request_helper.js');
 
-const Munros = function () {};
+const Munros = function () {
+  this.munros = null;
+};
 
 Munros.prototype.bindEvents = function () {
   PubSub.subscribe('SelectView:get-names', () => {
-    console.log('subscribe.bindEvents');
-
+    this.getData();
   });
 };
 
 Munros.prototype.getData = function () {
   const url = 'https://munroapi.herokuapp.com/api/munros';
+  const request = new RequestHelper(url);
 
+  request.get((munros) => {
+    this.munros = munros;
+    const munroNames = this.munros.map((munro) => {
+      return munro.name;
+    });
 
+    PubSub.publish('Munros:munro-name', munroNames);
+  });
 };
+
 module.exports = Munros;
--- src/views/select_view.js
+++ src/views/select_view.js
@@ -5,12 +5,29 @@ const SelectView = function (selectElement) {
 };
 
 SelectView.prototype.bindEvents = function () {
-
+  PubSub.subscribe('Munros:munro-name', (event) => {
+    const munroNames = event.detail;
+    this.populate(munroNames);
+  });
 
 };
 
 SelectView.prototype.getNames = function () {
-PubSub.publish('SelectView:get-names')
+  PubSub.publish('SelectView:get-names')
+};
+
+SelectView.prototype.populate = function (munroNames) {
+  for (const munroName of munroNames) {
+    this.element.appendChild(createElement('option', munroName));
+  }
+
+};
+
+function createElement(elementType, text) {
+  const element = document.createElement(elementType)
+  element.id = text
+  element.textContent = text
+  return element;
 };
 
 module.exports = SelectView;
